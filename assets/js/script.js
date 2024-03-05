313fb4c5c2499215d0ce69c88843d0a4d913fc6c
Zach Harvey <harveyzr@gmail.com>
2024-03-04 23:00:33 -0500

    Ready to deploy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,175 +1,132 @@
-// array of the quiz (question, choices, hit)
-var quizArray = [
+var questionSe = [
 	{
-		question:
-			"What is the HTML tag under which one can write the JavaScript code?",
+		question: "Which HTML element is used for specifying a footer for a document or section?",
 		answers: [
 			{
-				choice: "A. javascript",
+				choice: "A. bottom",
 				id: "A",
 			},
 			{
-				choice: "B. scripted",
+				choice: "B. footer",
 				id: "B",
 			},
 			{
-				choice: "C. script",
+				choice: "C. section",
 				id: "C",
 			},
 			{
-				choice: "D. js",
+				choice: "D. end",
 				id: "D",
 			},
+		],
+		hit: "B",
+	},
+
+	{
+		question: "In CSS, how do you select an element with the id 'unique'?",
+		answers: [
+			{ choice: "A. .unique", id: "A" },
+			{ choice: "B. #unique", id: "B" },
+			{ choice: "C. unique", id: "C" },
+			{ choice: "D. *unique", id: "D" },
+		],
+		hit: "B",
+	},
+
+	{
+		question: "How can you add a comment in a CSS file?",
+		answers: [
+			{ choice: "A. // This is a comment", id: "A" },
+			{ choice: "B. /* This is a comment */", id: "B" },
+			{ choice: "C. - This is a comment -", id: "C" },
+			{ choice: "D. <!-- This is a comment -->", id: "D" },
+		],
+		hit: "B",
+	},
+
+	{
+		question: "What does HTML stand for?",
+		answers: [
+			{ choice: "A. Hyper Trainer Marking Language", id: "A" },
+			{ choice: "B. Hyper Text Marketing Language", id: "B" },
+			{ choice: "C. Hyper Text Markup Language", id: "C" },
+			{ choice: "D. Hyper Text Markup Leveler", id: "D" },
 		],
 		hit: "C",
 	},
 
 	{
-		question: "How do you create a function in JavaScript?",
+		question: "Which object in JavaScript can be used to ensure code runs after a specified time delay?",
 		answers: [
-			{ choice: "A. function:myFunction()", id: "A" },
-			{ choice: "B. function myFunction()", id: "B" },
-			{ choice: "C. function = myFunction()", id: "C" },
-			{ choice: "D. None of the above.", id: "D" },
-		],
-		hit: "B",
-	},
-
-	{
-		question:
-			"Which built-in method combines the text of two strings and returns a new string?",
-		answers: [
-			{ choice: "A. append()", id: "A" },
-			{ choice: "B. concat()", id: "B" },
-			{ choice: "C. attach()", id: "C" },
-			{ choice: "D. None of the above.", id: "D" },
-		],
-		hit: "B",
-	},
-
-	{
-		question:
-			"Which of the following function of String object returns the characters in a string between two indexes into the string?",
-		answers: [
-			{ choice: "A. split()", id: "A" },
-			{ choice: "B. slice()", id: "B" },
-			{ choice: "C. substr()", id: "C" },
-			{ choice: "D. substring()", id: "D" },
-		],
-		hit: "D",
-	},
-
-	{
-		question: "How to write an IF statement in JavaScript?",
-		answers: [
-			{ choice: "A.  if i = 5", id: "A" },
-			{ choice: "B.  if (i == 5)", id: "B" },
-			{ choice: "C.  if i == 5 then", id: "C" },
-			{ choice: "D.  if i = 5 then", id: "D" },
-		],
-		hit: "B",
-	},
-
-	{
-		question: "Which of the following is not a valid JavaScript variable name?",
-		answers: [
-			{ choice: "A. 2names", id: "A" },
-			{ choice: "B. _first_and_last_names", id: "B" },
-			{ choice: "C.  FirstAndLast", id: "C" },
-			{ choice: "D. None of the above", id: "D" },
+			{ choice: "A. setTimeout", id: "A" },
+			{ choice: "B. timeWait", id: "B" },
+			{ choice: "C. delayFunction", id: "C" },
+			{ choice: "D. wait", id: "D" },
 		],
 		hit: "A",
 	},
+
+	{
+		question: "In CSS, what property is used to change the text color of an element?",
+		answers: [
+			{ choice: "A. text-color", id: "A" },
+			{ choice: "B. fgcolor", id: "B" },
+			{ choice: "C. color", id: "C" },
+			{ choice: "D. background-color", id: "D" },
+		],
+		hit: "C",
+	},
 ];
 
-//var index to go through the array
-var indexArray = 0;
+var questionIndex = 0;
+var playerScore = 0;
+var countdownTimer = 0;
+var highscoreList = [];
+if (localStorage.getItem("highscoreList") !== null) {
+	highscoreList = JSON.parse(localStorage.getItem("highscoreList"));
 
-//var for viewscores
-var score = 0;
-
-//var timer
-var timer = 0;
-
-//var array for highscores (array of objects)
-var userHighScores = [];
-if (localStorage.getItem("userHighScores") !== null) {
-	userHighScores = JSON.parse(localStorage.getItem("userHighScores"));
 }
-
 //var to check if in the middle of the quiz when the time is close to 0 the user select something
-var inProgress = false;
+var quizInProgress = false;
 
-//var to access the button element *start the quiz*
-var startEl = document.querySelector("#start");
-
-//var to access the welcome section
-var sectionWelcome = document.querySelector("#welcome-modal");
-//var to access the question section
-
-var sectionQuiz = document.querySelector("#modal-section");
-//var creating attribute to show the questions
-
-var questionEl = document.querySelector(".question-modal");
-//var options-modal
-
-var optionsEl = document.querySelector(".options-modal");
-//var hit selector
-
-var hitShow = document.querySelector(".hit");
-//creating a button for the choices
-
-var btnEl = document.createElement("BUTTON");
-//var modal done page
-
-var modalDoneEl = document.querySelector("#modal-done");
-//var score element
-
-var scoreEl = document.querySelector("#finalscore");
-//var to get submit button element
-
-var submitEl = document.querySelector("#submit");
-//timer element html
-
-var timerEl = document.querySelector("#count");
-//var to get highscoresPage section
-
-var highscoresPageEl = document.querySelector("#highscoresPage");
-//var for the initial elements
-
-var initialEl = document.querySelector("#initials");
-//var to show the initial and scores
-
-var showInitialScores = document.querySelector("#scores-list");
-//var submitGoback element
-
-var submitGobackEl = document.querySelector("#submitGoback");
-//var clear highscores
-
-var clearHighScoresEl = document.querySelector("#submitClear");
-//var to show array highscores
-
-var showHighScoresCorner = document.querySelector("#view-scores-id");
+var startButton = document.querySelector("#gamma-start");
+var welcomeSection = document.querySelector("#beta-modal");
+var questionSection = document.querySelector("#delta-section");
+var questionElement = document.querySelector(".question-modal");
+var optionElement = document.querySelector(".options-modal");
+var answerFeedback = document.querySelector(".hit");
+var completionModal = document.querySelector("#epsilon-done");
+var finalScoreElement = document.querySelector("#zeta-score");
+var submitButton = document.querySelector("#iota-submit");
+var timerDisplay = document.querySelector("#omega-span");
+var highscoresSection = document.querySelector("#kappa-highscores");
+var initialsInput = document.querySelector("#theta-initials");
+var highscoresDisplay = document.querySelector("#lambda-list");
+var goBackButton = document.querySelector("#mu-back");
+var clearHighscoresButton = document.querySelector("#nu-clear");
+var highscoresButton = document.querySelector("#alpha-btn");
+var hitShow = document.querySelector(".hit")
 
 /**
  * start the quiz
  */
-function beginQuiz() {
+function startQuiz() {
 	//reset quiz variables
 	indexArray = 0;
 	score = 0;
 	inProgress = true;
 	timer = 60;
-	showHighScoresCorner.setAttribute("disabled", "true");
+	highscoresButton.setAttribute("disabled", "true");
 
 	quizTimer();
-    sectionWelcome.classList.remove("d-flex");
-	sectionWelcome.classList.add("d-none");
+	//hidden the section when the button is pressed
+	welcomeSection.classList.remove("d-flex");
+	welcomeSection.classList.add("d-none");
 
 	//show the sectionQuiz with the add class d-flex
-	sectionQuiz.classList.add("d-flex");
+	questionSection.classList.add("d-flex");
 	//remove the sectionQuiz with the remove class d-none
-	sectionQuiz.classList.remove("d-none");
+	questionSection.classList.remove("d-none");
 	showPage();
 }
 
@@ -178,15 +135,15 @@ function beginQuiz() {
  */
 function cleanPage() {
 	// if (indexArray >= 0) {
-	questionEl.textContent = "";
-	optionsEl.textContent = "";
-	hitShow.textContent = "";
+	questionElement.textContent = "";
+	optionElement.textContent = "";
+	answerFeedback.textContent = "";
 
 	if (showCornerScores) {
-		sectionWelcome.classList.remove("d-flex");
-		sectionWelcome.classList.add("d-none");
-		modalDoneEl.classList.remove("d-flex");
-		modalDoneEl.classList.add("d-none");
+		welcomeSection.classList.remove("d-flex");
+		welcomeSection.classList.add("d-none");
+		completionModal.classList.remove("d-flex");
+		completionModal.classList.add("d-none");
 	} else {
 		return;
 	}
@@ -194,19 +151,19 @@ function cleanPage() {
 }
 
 function showPage() {
-	if (!inProgress || indexArray === quizArray.length) return;
+	if (!inProgress || indexArray === questionSe.length) return;
 	//add a margin-bottom to the questions
-	questionEl.classList.add("mb-3");
+	questionElement.classList.add("mb-3");
 	//show the question
-	questionEl.textContent = quizArray[indexArray].question;
+	questionElement.textContent = questionSe[indexArray].question;
 
-	for (let a = 0; a < quizArray[indexArray].answers.length; a++) {
+	for (let a = 0; a < questionSe[indexArray].answers.length; a++) {
 		var btnEl = document.createElement("BUTTON");
 		btnEl.classList.add("btn", "btn-info", "px-5", "mb-3", "mt-2", "text-left");
-		btnEl.textContent = quizArray[indexArray].answers[a].choice;
-		optionsEl.classList.add("d-flex", "flex-column");
-		optionsEl.appendChild(btnEl);
-		if (quizArray[indexArray].answers[a].id === quizArray[indexArray].hit) {
+		btnEl.textContent = questionSe[indexArray].answers[a].choice;
+		optionElement.classList.add("d-flex", "flex-column");
+		optionElement.appendChild(btnEl);
+		if (questionSe[indexArray].answers[a].id === questionSe[indexArray].hit) {
 			btnEl.addEventListener("click", timeDelayCorrect);
 		} else {
 			btnEl.addEventListener("click", timeDelayWrong);
@@ -218,17 +175,17 @@ function timeDelayCorrect() {
 	score++;
 	indexArray++;
 	cleanPage();
-	hitShow.classList.add("mt-4");
-	hitShow.textContent = "Rigth Answer!";
+	answerFeedback.classList.add("mt-4");
+	answerFeedback.textContent = "Rigth Answer!";
 	showPage();
 }
 
 function timeDelayWrong() {
-	timer = timer - 1;
+	timer = timer - 10;
 	indexArray++;
 	cleanPage();
-	hitShow.classList.add("mt-4");
-	hitShow.textContent = "Wrong Answer! Keep trying";
+	answerFeedback.classList.add("mt-4");
+	answerFeedback.textContent = "Wrong Answer! Keep trying";
 	showPage();
 }
 
@@ -238,9 +195,9 @@ function quizTimer() {
 
 		if (timer < 0) timer = 0;
 
-		timerEl.textContent = timer;
+		timerDisplay.textContent = timer;
 
-		if (timer <= 0 || indexArray === quizArray.length) {
+		if (timer <= 0 || indexArray === questionSe.length) {
 			clearInterval(timeId);
 			completeQuiz();
 		}
@@ -251,32 +208,32 @@ function completeQuiz() {
 	timer = 0;
 	inProgress = false;
 	cleanPage();
-	showHighScoresCorner.removeAttribute("disabled");
-	modalDoneEl.classList.remove("d-none");
-	modalDoneEl.classList.add("d-flex");
-	scoreEl.textContent = score;
+	highscoresButton.removeAttribute("disabled");
+	completionModal.classList.remove("d-none");
+	completionModal.classList.add("d-flex");
+	finalScoreElement.textContent = score;
 
-	if (initialEl.value !== "") {
-		initialEl.value = "";
+	if (initialsInput.value !== "") {
+		initialsInput.value = "";
 	}
 }
 
 function saveInitialScore() {
 	//getting the input and removing the outside spaces
-	var initialValue = initialEl.value.trim();
+	var initialValue = initialsInput.value.trim();
 
 	//force the user to enter the initial
 	if (
 		initialValue !== "" &&
-		typeof initialEl.value === "string" &&
+		typeof initialsInput.value === "string" &&
 		initialValue.toLowerCase().match(/^[a-z]+$/)
 	) {
-		userHighScores.push({ initials: initialValue, score: score });
-		localStorage.setItem("userHighScores", JSON.stringify(userHighScores));
+		highscoreList.push({ initials: initialValue, score: score });
+		localStorage.setItem("userHighScores", JSON.stringify(highscoreList));
 
 		//clean the page before highscores display page
-		modalDoneEl.classList.add("d-none");
-		modalDoneEl.classList.remove("d-flex");
+		completionModal.classList.add("d-none");
+		completionModal.classList.remove("d-flex");
 
 		// show the new page for highcores
 		showHighscores();
@@ -286,38 +243,39 @@ function saveInitialScore() {
 }
 
 function showHighscores() {
-	highscoresPageEl.classList.remove("d-none");
-	highscoresPageEl.classList.add("d-flex");
+	highscoresSection.classList.remove("d-none");
+	highscoresSection.classList.add("d-flex");
 
-	showInitialScores.textContent = "";
+	highscoresDisplay.textContent = "";
 
-	for (let i = 0; i < userHighScores.length; i++) {
+	for (let i = 0; i < highscoreList.length; i++) {
 		var divEl = document.createElement("div");
 		divEl.classList.add("card", "initial-score", "pl-2", "shadow-sm", "mb-2");
 		divEl.textContent =
 			i +
 			1 +
 			". " +
-			userHighScores[i].initials +
+			highscoreList[i].initials +
 			" - " +
-			userHighScores[i].score;
-		showInitialScores.appendChild(divEl);
+			highscoreList[i].score;
+			highscoresDisplay.appendChild(divEl);
 	}
 }
 
 function goBack() {
 	timer = 0;
-	timerEl.textContent = timer;
+	timerDisplay.textContent = timer;
 
-	highscoresPageEl.classList.add("d-none");
-	highscoresPageEl.classList.remove("d-flex");
-	sectionWelcome.classList.add("d-flex");
+	highscoresSection.classList.add("d-none");
+	highscoresSection.classList.remove("d-flex");
+	welcomeSection.classList.add("d-flex");
+	welcomeSection.classList.remove("d-none");
 }
 
 function clearScores() {
-	userHighScores.splice(0, userHighScores.length);
+	highscoreList.splice(0, highscoreList.length);
 	localStorage.removeItem("userHighScores");
-	showInitialScores.textContent = "";
+	highscoresDisplay.textContent = "";
 }
 
 function showCornerScores() {
@@ -326,8 +284,8 @@ function showCornerScores() {
 }
 
 // all event here
-startEl.addEventListener("click", beginQuiz);
-submitEl.addEventListener("click", saveInitialScore);
-submitGobackEl.addEventListener("click", goBack);
-clearHighScoresEl.addEventListener("click", clearScores);
-showHighScoresCorner.addEventListener("click", showCornerScores);
+startButton.addEventListener("click", startQuiz);
+submitButton.addEventListener("click", saveInitialScore);
+goBackButton.addEventListener("click", goBack);
+clearHighscoresButton.addEventListener("click", clearScores);
+highscoresButton.addEventListener("click", showCornerScores);
