99a6f5a88cb770b662c7f5b178c1f1b19604af3c
Zach Harvey <harveyzr@gmail.com>
2024-03-04 21:02:02 -0500

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# quizchallenge
+# Quiz Challenge
+
+## Table of Contents
+
+ - [Description](#description)
+ - [User Story](#user-story)
+ - [Acceptance Criteria](#acceptance-criteria)
+ - [Images](#images)
+ - [Link To Deployed Webpage](#link-to-deployed-webpage)
+ - [Link to GitHub Pages ](#link-to-gethub-website)
+ - [Skills Used](#skills-used)
+
+ ## Description
+The main goal of this application is to use the skills we learned in class to create a coding quiz.
+
+ Code quiz is the fourth challenge for the Ohio State coding bootcamp.
+-	My motivation for this project was to become proficient at coding HTML, CSS, Javascript, and bootstrap.  
+-	Building this project allowed me to implement coding techniques that I have been studying for the 6 weeks. 
+-	This page was a perfect example of real-world practices and solved the problem of assignments with no starter code. 
+-	I learned how to code and deploy a simle webpage with nothing more than HTML, CSS, Java Script, and bootstrap.  
+
+
+ ## User Story
+- AS A coding boot camp student
+- I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+- SO THAT I can gauge my progress compared to my peers
+ 
+ ## Acceptance Criteria
+- GIVEN I am taking a code quiz
+- WHEN I click the start button
+- THEN a timer starts and I am presented with a question
+- WHEN I answer a question
+- THEN I am presented with another question
+- WHEN I answer a question incorrectly
+- THEN time is subtracted from the clock
+- WHEN all questions are answered or the timer reaches 0
+- THEN the game is over
+- WHEN the game is over
+- THEN I can save my initials and score
+ ## Images
+![Website Preview](assets/img/Screenshot%20Quiz.jpg)
+
+ ## Link to Deployed Webpage
+https://harveyzr.github.io/pass-gen/
+ 
+## Link to GetHub Website
+https://github.com/harveyzr/pass-gen
+
+ ## Skills Used
+- JavaScript
+- bootstrap
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
+// array of the quiz (question, choices, hit)
 var quizArray = [
 	{
 		question:
 			"What is the HTML tag under which one can write the JavaScript code?",
 		answers: [
 			{
-				choice: "A. <javascript>",
+				choice: "A. javascript",
 				id: "A",
 			},
 			{
-				choice: "B. <scripted>",
+				choice: "B. scripted",
 				id: "B",
 			},
 			{
-				choice: "C. <script>",
+				choice: "C. script",
 				id: "C",
 			},
 			{
-				choice: "D. <js>",
+				choice: "D. js",
 				id: "D",
 			},
 		],
@@ -81,30 +82,252 @@ var quizArray = [
 	},
 ];
 
+//var index to go through the array
+var indexArray = 0;
 
-var startE1 = document.querySelector("#start");
+//var for viewscores
+var score = 0;
 
-// start quiz function
-function startQuiz(){
-    indexArray = 0;
-    score = 0;
-    inProgress = true;
-    timer = 60;
-    showHighScoresCorner.setAttribute("disabled", "true");
+//var timer
+var timer = 0;
 
-    quizTimer();
+//var array for highscores (array of objects)
+var userHighScores = [];
+if (localStorage.getItem("userHighScores") !== null) {
+	userHighScores = JSON.parse(localStorage.getItem("userHighScores"));
+}
+
+//var to check if in the middle of the quiz when the time is close to 0 the user select something
+var inProgress = false;
+
+//var to access the button element *start the quiz*
+var startEl = document.querySelector("#start");
+
+//var to access the welcome section
+var sectionWelcome = document.querySelector("#welcome-modal");
+//var to access the question section
+
+var sectionQuiz = document.querySelector("#modal-section");
+//var creating attribute to show the questions
+
+var questionEl = document.querySelector(".question-modal");
+//var options-modal
+
+var optionsEl = document.querySelector(".options-modal");
+//var hit selector
+
+var hitShow = document.querySelector(".hit");
+//creating a button for the choices
+
+var btnEl = document.createElement("BUTTON");
+//var modal done page
+
+var modalDoneEl = document.querySelector("#modal-done");
+//var score element
+
+var scoreEl = document.querySelector("#finalscore");
+//var to get submit button element
+
+var submitEl = document.querySelector("#submit");
+//timer element html
+
+var timerEl = document.querySelector("#count");
+//var to get highscoresPage section
+
+var highscoresPageEl = document.querySelector("#highscoresPage");
+//var for the initial elements
+
+var initialEl = document.querySelector("#initials");
+//var to show the initial and scores
+
+var showInitialScores = document.querySelector("#scores-list");
+//var submitGoback element
+
+var submitGobackEl = document.querySelector("#submitGoback");
+//var clear highscores
+
+var clearHighScoresEl = document.querySelector("#submitClear");
+//var to show array highscores
+
+var showHighScoresCorner = document.querySelector("#view-scores-id");
+
+/**
+ * start the quiz
+ */
+function beginQuiz() {
+	//reset quiz variables
+	indexArray = 0;
+	score = 0;
+	inProgress = true;
+	timer = 60;
+	showHighScoresCorner.setAttribute("disabled", "true");
+
+	quizTimer();
     sectionWelcome.classList.remove("d-flex");
-    sectionWelcome.classList.add("d-none");
+	sectionWelcome.classList.add("d-none");
 
-    sectionQuiz.classList.add("d-flex");
-
-    sectionQuiz.classList.remove("d-none");
-    showPage();
+	//show the sectionQuiz with the add class d-flex
+	sectionQuiz.classList.add("d-flex");
+	//remove the sectionQuiz with the remove class d-none
+	sectionQuiz.classList.remove("d-none");
+	showPage();
 }
 
+/**
+ * clean the page after start button is pressed
+ */
 function cleanPage() {
-    questionE1.textContent
+	// if (indexArray >= 0) {
+	questionEl.textContent = "";
+	optionsEl.textContent = "";
+	hitShow.textContent = "";
+
+	if (showCornerScores) {
+		sectionWelcome.classList.remove("d-flex");
+		sectionWelcome.classList.add("d-none");
+		modalDoneEl.classList.remove("d-flex");
+		modalDoneEl.classList.add("d-none");
+	} else {
+		return;
+	}
+	// }
 }
 
-// Events
-startE1.addEventListener("click", startQuiz);
+function showPage() {
+	if (!inProgress || indexArray === quizArray.length) return;
+	//add a margin-bottom to the questions
+	questionEl.classList.add("mb-3");
+	//show the question
+	questionEl.textContent = quizArray[indexArray].question;
+
+	for (let a = 0; a < quizArray[indexArray].answers.length; a++) {
+		var btnEl = document.createElement("BUTTON");
+		btnEl.classList.add("btn", "btn-info", "px-5", "mb-3", "mt-2", "text-left");
+		btnEl.textContent = quizArray[indexArray].answers[a].choice;
+		optionsEl.classList.add("d-flex", "flex-column");
+		optionsEl.appendChild(btnEl);
+		if (quizArray[indexArray].answers[a].id === quizArray[indexArray].hit) {
+			btnEl.addEventListener("click", timeDelayCorrect);
+		} else {
+			btnEl.addEventListener("click", timeDelayWrong);
+		}
+	}
+}
+
+function timeDelayCorrect() {
+	score++;
+	indexArray++;
+	cleanPage();
+	hitShow.classList.add("mt-4");
+	hitShow.textContent = "Rigth Answer!";
+	showPage();
+}
+
+function timeDelayWrong() {
+	timer = timer - 1;
+	indexArray++;
+	cleanPage();
+	hitShow.classList.add("mt-4");
+	hitShow.textContent = "Wrong Answer! Keep trying";
+	showPage();
+}
+
+function quizTimer() {
+	var timeId = setInterval(() => {
+		timer = timer - 1;
+
+		if (timer < 0) timer = 0;
+
+		timerEl.textContent = timer;
+
+		if (timer <= 0 || indexArray === quizArray.length) {
+			clearInterval(timeId);
+			completeQuiz();
+		}
+	}, 1000);
+}
+
+function completeQuiz() {
+	timer = 0;
+	inProgress = false;
+	cleanPage();
+	showHighScoresCorner.removeAttribute("disabled");
+	modalDoneEl.classList.remove("d-none");
+	modalDoneEl.classList.add("d-flex");
+	scoreEl.textContent = score;
+
+	if (initialEl.value !== "") {
+		initialEl.value = "";
+	}
+}
+
+function saveInitialScore() {
+	//getting the input and removing the outside spaces
+	var initialValue = initialEl.value.trim();
+
+	//force the user to enter the initial
+	if (
+		initialValue !== "" &&
+		typeof initialEl.value === "string" &&
+		initialValue.toLowerCase().match(/^[a-z]+$/)
+	) {
+		userHighScores.push({ initials: initialValue, score: score });
+		localStorage.setItem("userHighScores", JSON.stringify(userHighScores));
+
+		//clean the page before highscores display page
+		modalDoneEl.classList.add("d-none");
+		modalDoneEl.classList.remove("d-flex");
+
+		// show the new page for highcores
+		showHighscores();
+	} else {
+		alert("you must to enter your initials to save your score!");
+	}
+}
+
+function showHighscores() {
+	highscoresPageEl.classList.remove("d-none");
+	highscoresPageEl.classList.add("d-flex");
+
+	showInitialScores.textContent = "";
+
+	for (let i = 0; i < userHighScores.length; i++) {
+		var divEl = document.createElement("div");
+		divEl.classList.add("card", "initial-score", "pl-2", "shadow-sm", "mb-2");
+		divEl.textContent =
+			i +
+			1 +
+			". " +
+			userHighScores[i].initials +
+			" - " +
+			userHighScores[i].score;
+		showInitialScores.appendChild(divEl);
+	}
+}
+
+function goBack() {
+	timer = 0;
+	timerEl.textContent = timer;
+
+	highscoresPageEl.classList.add("d-none");
+	highscoresPageEl.classList.remove("d-flex");
+	sectionWelcome.classList.add("d-flex");
+}
+
+function clearScores() {
+	userHighScores.splice(0, userHighScores.length);
+	localStorage.removeItem("userHighScores");
+	showInitialScores.textContent = "";
+}
+
+function showCornerScores() {
+	cleanPage();
+	showHighscores();
+}
+
+// all event here
+startEl.addEventListener("click", beginQuiz);
+submitEl.addEventListener("click", saveInitialScore);
+submitGobackEl.addEventListener("click", goBack);
+clearHighScoresEl.addEventListener("click", clearScores);
+showHighScoresCorner.addEventListener("click", showCornerScores);
